1dd82a63632a12c8cf40cd4b86c2ec6484a435cb
Wasis Adi Putranto <wasis.putra@gmail.com>
2025-08-02 16:54:33 +0700

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -17,11 +17,33 @@ module "app_service" {
   name        = "golden-phoenix-app"
   cluster_arn = module.ecs_cluster.arn
 
-  cpu    = 1024
-  memory = 4096
+  cpu    = 512
+  memory = 2048
+
+  # Container definition(s)
+  container_definitions = {
+    app = {
+      essential = true
+      image     = "public.ecr.aws/aws-containers/ecsdemo-frontend:776fd50"
+      portMappings = [
+        {
+          name          = "http"
+          containerPort = 80
+          protocol      = "tcp"
+        }
+      ]
+
+      # Example image used requires access to write to root filesystem
+      readonlyRootFilesystem = false
+      memoryReservation      = 100
+
+      restartPolicy = {
+        enabled = true
+        ignoredExitCodes = [1]
+        restartAttemptPeriod = 60
+      }
+    }
+  }
 
   subnet_ids = module.vpc.private_subnets
-
-  create_task_definition = false
-  task_definition_arn   = 
 }
