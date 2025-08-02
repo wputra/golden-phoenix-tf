2c47e0159f1048e933559f8c4a03c55cce4ff24c
Wasis Adi Putranto <wasis.putra@gmail.com>
2025-08-02 16:53:10 +0700

    app

--- ecs.tf
+++ ecs.tf
@@ -10,3 +10,18 @@ module "ecs_cluster" {
     }
   }
 }
+
+module "app_service" {
+  source = "terraform-aws-modules/ecs/aws//modules/service"
+
+  name        = "golden-phoenix-app"
+  cluster_arn = module.ecs_cluster.arn
+
+  cpu    = 1024
+  memory = 4096
+
+  subnet_ids = module.vpc.private_subnets
+
+  create_task_definition = false
+  task_definition_arn   = 
+}
