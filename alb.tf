163c11c5684dad36b200df27b7fbeda3aa858fa0
Wasis Adi Putranto <wasis.putra@gmail.com>
2025-08-07 10:09:47 +0700

    https

--- alb.tf
+++ alb.tf
@@ -50,3 +50,15 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app.arn
   }
 }
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = module.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.this.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
